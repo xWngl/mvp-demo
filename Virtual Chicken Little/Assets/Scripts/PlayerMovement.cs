d967a9cabc9f512b4c125bb828da708a8f3d4274
Uguudei Bayaraa <uguudei.bayar@gmail.com>
2021-12-06 18:30:45 -0600

    level1 started:

---
+++ Virtual Chicken Little/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    private Rigidbody2D body;
+    public float speed, bound1, bound2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+
+        if (horizontal > 0)
+        {
+          body.velocity = Vector2.right * speed;
+        } else if (horizontal < 0)
+        {
+          body.velocity = Vector2.left * speed;
+        } else 
+        {
+          body.velocity = Vector2.zero;
+        }
+
+        transform.position = new Vector2(Mathf.Clamp(transform.position.x, bound1, bound2), transform.position.y);
+    }
+}
