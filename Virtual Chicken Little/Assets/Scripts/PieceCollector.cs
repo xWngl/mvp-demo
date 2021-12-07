ec31e5943fd5594a196ae03360cabc8c49f52b5f
Uguudei Bayaraa <uguudei.bayar@gmail.com>
2021-12-06 22:16:00 -0600

    level 1

---
+++ Virtual Chicken Little/Assets/Scripts/PieceCollector.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceCollector : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D target)
+    {
+      Destroy(target.gameObject);
+    }
+}
--- Virtual Chicken Little/Assets/Scripts/PuzzleSpawner.cs
+++ Virtual Chicken Little/Assets/Scripts/PuzzleSpawner.cs
@@ -6,16 +6,24 @@ public class PuzzleSpawner : MonoBehaviour
 {
     public GameObject[] puzzles;
     public float xbound1, xbound2, ybound;
-    
+
     // Start is called before the first frame update
     void Start()
     {
-        
+      StartCoroutine(SpawnRandomPuzzlePieces());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SpawnRandomPuzzlePieces()
     {
-        
+      yield return new WaitForSeconds(Random.Range(1, 4));
+      int rPuzzle = Random.Range(0, puzzles.Length);
+
+      if(Random.value <= .5f)
+      {
+        Instantiate(puzzles[rPuzzle], 
+          new Vector2(Random.Range(xbound1, xbound2), ybound), Quaternion.identity);
+      }
+
+      StartCoroutine(SpawnRandomPuzzlePieces());
     }
 }
